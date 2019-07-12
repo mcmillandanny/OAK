12c80b8740acc32bb26d4c608a46cf6422a28a9f
Dnny <mcmillandanny@gmail.com>
2019-07-12 16:17:23 -0400

    polished off some styles / fixed sizing

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,15 +4,14 @@ var GoogleMapModule = function () {
 
     var map;
     function initMap() {
-        var markerPostion = { lat: 33.7489954, lng: -84.3879824 };
+        var markerPostion = { lat: 35.522610, lng: -97.548180 };
         map = new google.maps.Map(document.getElementById('map'), {
             center: markerPostion,
-            zoom: 8
-
+            zoom: 11
         });
 
         var infoWindow = new google.maps.InfoWindow({
-            content: "Welcome!!"
+            content: "OAK"
         });
         var marker = new google.maps.Marker({
             position: markerPostion,
